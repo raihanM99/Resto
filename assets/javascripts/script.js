ad0d10e13feb483a0f77731b0d5f40b8b39edbc5
Adi Yudhanegara <adi.yudhanegaradona@gmail.com>
2022-08-04 21:42:48 +0800

    Push aja

---
+++ assets/javascripts/script.js
@@ -0,0 +1,23 @@
+$(function() {
+  $('.owl-carousel').owlCarousel({
+    items:   1,
+    loop:   true,
+    margin:  40,
+    dots:    false,
+    nav:     false,
+    stagePadding: 100,
+  });
+  
+  function responsive_content() {
+    var carousel_content = $('.restaurants-carousel > div');
+    $('.owl-stage').css('padding-left', '');
+    $('.restaurants-carousel').css('height', `${carousel_content.height()}px`);
+  }
+
+  responsive_content();
+  $(window).on('changed.owl.carousel', function(event) {
+    setTimeout(function() {
+      responsive_content();
+    });
+  });
+});
